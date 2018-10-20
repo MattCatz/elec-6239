796e521e999b58c36b148e609ab0965ff701b7f7
Matthew <14895427+MattCatz@users.noreply.github.com>
2018-10-20 12:40:43 -0700

    Got it to work without segfault

--- mandelbrot/main.c
+++ mandelbrot/main.c
@@ -12,26 +12,29 @@
   */
  #include <stdio.h>
  #include <math.h>
- int main()
- {
-   FILE * fp;
-   char *filename="new1.ppm";
-   char *comment="# ";/* comment should start with # */
-   static unsigned char color[3];
-
-   /*create new file,give it a name and open it in binary mode  */
-   fp= fopen(filename,"wb"); /* b -  binary mode */
-   /*write ASCII header to the file*/
-   fprintf(fp,"P6\n %s\n %d\n %d\n %d\n",comment,iXmax,iYmax,MaxColorComponentValue);
-   /* compute and write image data bytes to the file*/
-   
-   void calc_mandel(fp)
-   
-   fclose(fp);
-   return 0;
- }
+ #include <stdlib.h>
  
+struct {
+   int iXmax,iYmax;
+   double CxMin,CxMax,CyMin,CyMax;
+   int IterationMax;
+   int EscapeRadius;
+} typedef mandelbrot_t;
+
+struct {
+   unsigned char r,g,b;
+} typedef rgb_t;
+ 
+rgb_t **tex = 0;
+
 void hsv_to_rgb(int hue, int min, int max, rgb_t *p) {
+   int invert, saturation, color_rotate, VAL;
+   
+   invert = 0;
+   saturation = 1;
+   color_rotate = 0;
+   VAL = 255;
+   
 	if (min == max) max = min + 1;
 	if (invert) hue = max - (hue - min);
 	if (!saturation) {
@@ -54,102 +57,113 @@ void hsv_to_rgb(int hue, int min, int max, rgb_t *p) {
 	}
 }
  
-void calc_mandel(FILE *fp) {
-    /* screen ( integer) coordinate */
+void calc_mandel(mandelbrot_t *m, FILE *fp) {
+    /* screen (integer) coordinate */
    int iX,iY;
-   const int iXmax = 1200;
-   const int iYmax = 1200;
-   /* world ( double) coordinate = parameter plane*/
+   const int iXmax = m->iXmax;
+   const int iYmax = m->iYmax;
+   
+   /* world (double) coordinate = parameter plane*/
    double Cx,Cy;
-   const double CxMin=-2.5;
-   const double CxMax=1.5;
-   const double CyMin=-2.0;
-   const double CyMax=2.0;
+   const double CxMin = m->CxMin;
+   const double CxMax = m->CxMax;
+   const double CyMin = m->CyMin;
+   const double CyMax = m->CyMax;
    /* */
    double PixelWidth=(CxMax-CxMin)/iXmax;
    double PixelHeight=(CyMax-CyMin)/iYmax;
-   /* color component ( R or G or B) is coded from 0 to 255 */
-   /* it is 24 bit color RGB file */
-   const int MaxColorComponentValue=255;
    
+   // Used to get even gradient
+   int min,max;
+   min = max = 0;
+   rgb_t *px;
+
    /* Z=Zx+Zy*i  ;   Z0 = 0 */
    double Zx, Zy;
    double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
    /*  */
    int Iteration;
-   const int IterationMax=100000;
+   const int IterationMax = m->IterationMax;
    /* bail-out value , radius of circle ;  */
-   const double EscapeRadius=5;
+   const double EscapeRadius = m->EscapeRadius;
    double ER2=EscapeRadius*EscapeRadius;
    
-   for(iY=0;iY<iYmax;iY++)
-  {
- Cy=CyMin + iY*PixelHeight;
- if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
- for(iX=0;iX<iXmax;iX++)
- {
-            Cx=CxMin + iX*PixelWidth;
-            /* initial value of orbit = critical point Z= 0 */
-            Zx=0.0;
-            Zy=0.0;
+   for(iY=0;iY<iYmax;iY++) {
+      px = tex[iY];
+      Cy=CyMin + iY*PixelHeight;
+      if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
+      for(iX=0;iX<iXmax;iX++) {
+         Cx=CxMin + iX*PixelWidth;
+         /* initial value of orbit = critical point Z= 0 */
+         Zx=0.0;
+         Zy=0.0;
+         Zx2=Zx*Zx;
+         Zy2=Zy*Zy;
+         /* */
+         for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++) {
+            Zy=2*Zx*Zy + Cy;
+            Zx=Zx2-Zy2 +Cx;
             Zx2=Zx*Zx;
             Zy2=Zy*Zy;
-            /* */
-            for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
-            {
-                Zy=2*Zx*Zy + Cy;
-                Zx=Zx2-Zy2 +Cx;
-                Zx2=Zx*Zx;
-                Zy2=Zy*Zy;
-            };
-            /* compute  pixel color (24 bit = 3 bytes) */
-            if (Iteration==IterationMax)
-            { /*  interior of Mandelbrot set = black */
-               color[0]=0;
-               color[1]=0;
-               color[2]=0;
-            }
-         else
-            { /* exterior of Mandelbrot set = white */
-                 color[0]=255; /* Red*/
-                 color[1]=255;  /* Green */
-                 color[2]=255;/* Blue */
-            };
-            /*write color to the file*/
-            fwrite(color,1,3,fp);
-    }
-  }
-   
-	for (i = 0; i < height; i++) {
-		px = tex[i];
-		y = (i - height/2) * scale + cy;
-		for (j = 0; j  < width; j++, px++) {
-			x = (j - width/2) * scale + cx;
-			iter = 0;
+         };
+         if (Iteration < min) min = Iteration;
+         if (Iteration > max) max = Iteration;
+         *(unsigned short *)px = Iteration;
+      }
+   }
+  
+   int i, j;
  
-			zx = hypot(x - .25, y);
-			if (x < zx - 2 * zx * zx + .25) iter = max_iter;
-			if ((x + 1)*(x + 1) + y * y < 1/16) iter = max_iter;
- 
-			zx = zy = zx2 = zy2 = 0;
-			for (; iter < max_iter && zx2 + zy2 < 4; iter++) {
-				zy = 2 * zx * zy + y;
-				zx = zx2 - zy2 + x;
-				zx2 = zx * zx;
-				zy2 = zy * zy;
-			}
-			if (iter < min) min = iter;
-			if (iter > max) max = iter;
-			*(unsigned short *)px = iter;
-		}
-	}
- 
-	for (i = 0; i < height; i++) {
-		for (j = 0, px = tex[i]; j  < width; j++, px++) {
+	for (i = 0; i < iXmax; i++) {
+		for (j = 0, px = tex[i]; j  < iYmax; j++, px++) {
 			hsv_to_rgb(*(unsigned short*)px, min, max, px);
 			fwrite(px,1,3,fp);
 		}
 	}
 	
 	
+}
+
+int main() {
+   FILE * fp;
+   char *filename="new1.ppm";
+   char *comment="# ";/* comment should start with # */
+   
+   mandelbrot_t m;
+   
+   m.iXmax = 1200;
+   m.iYmax = 1200;
+   
+   m.CxMin = -2.5;
+   m.CxMax = 1.5;
+   
+   m.CyMin = -2.0;
+   m.CyMax = 2.0;
+   
+   m.IterationMax=100000;
+   m.EscapeRadius=5;
+   
+   /* color component ( R or G or B) is coded from 0 to 255 */
+   /* it is 24 bit color RGB file */
+   const int MaxColorComponentValue=255;
+   
+   tex = malloc(sizeof(rgb_t *) * m.iXmax);
+   tex[0] = malloc(sizeof(rgb_t) * m.iXmax * m.iYmax);
+
+   int i;
+
+   for(i = 0; i < m.iXmax; i++) 
+        tex[i] = (*tex + m.iYmax * i);
+   
+   /*create new file,give it a name and open it in binary mode  */
+   fp= fopen(filename,"wb"); /* b -  binary mode */
+   
+   /*write ASCII header to the file*/
+   fprintf(fp,"P6\n %s\n %d\n %d\n %d\n",comment,m.iXmax,m.iYmax,MaxColorComponentValue);
+   /* compute and write image data bytes to the file*/
+   
+   calc_mandel(&m, fp);
+   
+   fclose(fp);
+   return 0;
 }
