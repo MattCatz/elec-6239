6de009655fa6bf2a6cf0fcbc40e508d8647b87c9
mattcatz <14895427+MattCatz@users.noreply.github.com>
2018-10-24 23:23:46 -0500

    Got partitioning to work

--- project-7/hello.c
+++ project-7/hello.c
@@ -24,15 +24,11 @@ int main(int argc, char** argv) {
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
-
     /* Program begins */
 
     int M,W,source,dest;
     int j,k,chunk,window,offset;
-    M = 40;
+    M = 12;
     W = 3;
 
     // Communication buffers
@@ -45,19 +41,18 @@ int main(int argc, char** argv) {
     chunk = M / world_size;
     window = chunk + (W-1);
 
-    double *filter,*i;
+    double *filter,*p;
 
     filter = calloc(W*W, sizeof(double));
     assert(filter != NULL);
 
     // Generating filter
-    for (i = &filter[(W*W)-1]; i != filter-1; i--) {
-      *i = 1.0/(pow(W,2));
+    for (p = &filter[(W*W)-1]; p != filter-1; p--) {
+      *p = 1.0/(pow(W,2));
     }
 
     if (world_rank == 0) {
         double *image;
-        double *i;
 
         printf("Thread 0 setting up some stuff\n");
 
@@ -65,62 +60,68 @@ int main(int argc, char** argv) {
         assert(image != NULL);
 
         // Generating image
-        for (i = &image[(M*M)-1]; i != image-1; i--) {
-          *i = 1089;//j % 2 ? 0 : 1089;
+        p = image;
+        for (j = M*M; j > 0; j-=2) {
+          *p = 1;//1089;
+          p += 2;
         }
-
-        // Generating filter
-        // for (i = filter + (W*W); i != filter; i--) {
-        //   *i = 1.0/(pow(W,2));
-        // }
 
         // Start at 1 because I dont have to send to myself
         for (dest = 1; dest < world_size; dest++) {
-          offset = (dest * chunk);
+          offset = (dest * chunk)*M;
           printf("Sending out partition %d to thread %d\n",offset,dest);
-          MPI_Send(&image[offset], 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD); //image
+          MPI_Send(&image[offset], window*M, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD); //image
         }
 
         //convolve(image, filter);
         printf("Master thread doing convolution\n");
 
         for (j = 1; j < world_size; j++) {
-          source = j,
-          MPI_Recv(&k, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          source = j;
+          offset = (source * chunk)*M;
+          MPI_Recv(&image[offset], window*M, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           printf("Master thread %d got back %d\n", world_rank, k);
         }
 
-        for (j = 0; j < 40; j++) {
-          for (k = 0; k < 40; k++) {
-             printf("%.0f, ", INDEX(image,M,j,k)); // Notice only 3 digits
+        for (j = 0; j < M; j++) {
+          for (k = 0; k < M; k++) {
+             printf("%4.0f, ", INDEX(image,M,j,k)); // Notice only 3 digits
           }
           printf("\n");
         }
-      } else {
+      } else { // Begin not master node stuff
         buffer = calloc(window*M, sizeof(double));
         assert(buffer != NULL);
 
         result = calloc(chunk*M, sizeof(double));
         assert(result != NULL);
 
-        int b;
-
-        printf("Thread %d waiting on partition\n", world_rank);
         source = 0; // Master node
-        MPI_Recv(buffer, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(buffer, window*M, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        printf("Thread %f got partition\n", *buffer);
-        // for (j = 0; j < 10; j++) {
-        //   for (k = 0; k < 10; k++) {
-        //      //printf("%.3f, ", INDEX(buffer,M,j,k)); // Notice only 3 digits
+        // for (j = 0; j < chunk; j++) {
+        //   for (k = 0; k < M; k++) {
+        //     INDEX(result,M,j,k) = INDEX(buffer,M,j+(W/2),k) + world_rank;
         //   }
-        //   printf("\n");
         // }
 
+        int half = W/2;
+        int i,l;
+
+        for(i=0; i < chunk; ++i) {
+          for(j=0; j < M; ++j) {
+            for(k=-half; k <= half; ++k) {
+              for(l=-half; l <= half; ++l) {
+                if( (i-k >= -half && i-k < window + half) && (j-l >= 0 && j-l < M) ) {
+                  result[(M*i) + j] += world_rank;//buffer[M*(i-k+half)+(j-l)]*filter[W*(k+half)+(l+half)];
+                }
+              }
+            }
+          }
+        }
+
         dest = 0;
-        //MPI_Send(&b, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-        MPI_Send(&b, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-        printf("Thread %d done\n", world_rank);
+        MPI_Send(result, chunk*M, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
       }
 
     /* Program ends */
