5b4a7264af2e9770cfd18d072f84b26d141cb15c
mattcatz <14895427+MattCatz@users.noreply.github.com>
2018-10-25 00:52:09 -0500

    Got the non-master threads doing convolution.

--- project-7/hello.c
+++ project-7/hello.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include <mpi.h>
 #include <stdio.h>
@@ -28,8 +29,12 @@ int main(int argc, char** argv) {
 
     int M,W,source,dest;
     int j,k,chunk,window,offset;
-    M = 12;
+    int half;
+    int i,l;
+    M = 8;
     W = 3;
+
+    half = W/2;
 
     // Communication buffers
     double *buffer,*result;
@@ -46,6 +51,12 @@ int main(int argc, char** argv) {
     filter = calloc(W*W, sizeof(double));
     assert(filter != NULL);
 
+    buffer = calloc(window*M, sizeof(double));
+    assert(buffer != NULL);
+
+    result = calloc(chunk*M, sizeof(double));
+    assert(result != NULL);
+
     // Generating filter
     for (p = &filter[(W*W)-1]; p != filter-1; p--) {
       *p = 1.0/(pow(W,2));
@@ -60,60 +71,67 @@ int main(int argc, char** argv) {
         assert(image != NULL);
 
         // Generating image
-        p = image;
-        for (j = M*M; j > 0; j-=2) {
-          *p = 1;//1089;
-          p += 2;
-        }
+        // p = image;
+        // for (j = M*M; j > 0; j-=1) {
+        //   *p = 1;//1089;
+        //   p += 1;
+        // }
+
+        double test [64] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+         for (j = 0; j < 64; j++) {
+           image[j] = test[j];
+         }
+
+         // for (j = 0; j < M; j++) {
+         //   for (k = 0; k < M; k++) {
+         //      printf("%2.3f, ", INDEX(image,M,j,k)); // Notice only 3 digits
+         //   }
+         //   printf("\n");
+         // }
+
+         // printf("\n");
 
         // Start at 1 because I dont have to send to myself
         for (dest = 1; dest < world_size; dest++) {
-          offset = (dest * chunk)*M;
-          printf("Sending out partition %d to thread %d\n",offset,dest);
+          // We subtract half to get some of the stuff above us
+          offset = (dest * chunk - half)*M;
+          // printf("Sending out partition %d to thread %d\n",offset,dest);
           MPI_Send(&image[offset], window*M, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD); //image
         }
 
         //convolve(image, filter);
-        printf("Master thread doing convolution\n");
+        // printf("Master thread doing convolution\n");
 
         for (j = 1; j < world_size; j++) {
           source = j;
           offset = (source * chunk)*M;
-          MPI_Recv(&image[offset], window*M, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Master thread %d got back %d\n", world_rank, k);
+          MPI_Recv(&image[offset], chunk*M, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
         for (j = 0; j < M; j++) {
           for (k = 0; k < M; k++) {
-             printf("%4.0f, ", INDEX(image,M,j,k)); // Notice only 3 digits
+             printf("%2.3f, ", INDEX(image,M,j,k)); // Notice only 3 digits
           }
           printf("\n");
         }
       } else { // Begin not master node stuff
-        buffer = calloc(window*M, sizeof(double));
-        assert(buffer != NULL);
 
-        result = calloc(chunk*M, sizeof(double));
-        assert(result != NULL);
 
         source = 0; // Master node
         MPI_Recv(buffer, window*M, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        // for (j = 0; j < chunk; j++) {
-        //   for (k = 0; k < M; k++) {
-        //     INDEX(result,M,j,k) = INDEX(buffer,M,j+(W/2),k) + world_rank;
-        //   }
-        // }
 
-        int half = W/2;
-        int i,l;
+        /* Note that right now the bottom partition looks past the bottom of the
+           matrix but because our buffer is big enough and we zero it all out
+           there is no effect */
 
         for(i=0; i < chunk; ++i) {
           for(j=0; j < M; ++j) {
             for(k=-half; k <= half; ++k) {
               for(l=-half; l <= half; ++l) {
-                if( (i-k >= -half && i-k < window + half) && (j-l >= 0 && j-l < M) ) {
-                  result[(M*i) + j] += world_rank;//buffer[M*(i-k+half)+(j-l)]*filter[W*(k+half)+(l+half)];
+                if( (i-k >= -half && i-k <= window + half) && (j-l >= 0 && j-l < M) ) {
+                  result[(M*i) + j] += buffer[M*(i-k+half)+(j-l)]*filter[W*(k+half)+(l+half)];
                 }
               }
             }
